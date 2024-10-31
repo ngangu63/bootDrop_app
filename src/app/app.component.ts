c6a37a14cf2d797a36728a0fb110415dffbf3ebd
ngangu63 <banabalukala@gmail.com>
2024-10-30 18:46:04 -0700

    deleted Digireant component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyfooterComponent } from "./myfooter/myfooter.component";
 import { RouterModule } from '@angular/router';
+import { OthersMembersComponent } from './others-members/others-members.component';
+import { CommonModule } from '@angular/common';
+import { LesDirigeantsComponent } from './les-dirigeants/les-dirigeants.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyfooterComponent,RouterModule],
+  imports: [RouterOutlet, MyfooterComponent,RouterModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myDropDown-app';
+  isSearchEnabled: boolean = false;
+
+  toggleSearch(enable: boolean) {
+    this.isSearchEnabled = enable;
+  }
+
+
+  onChildActivate(componentRef: any) {
+    // Enable search only for certain child components
+    this.isSearchEnabled = componentRef instanceof LesDirigeantsComponent || componentRef instanceof OthersMembersComponent;
+    console.log(this.isSearchEnabled);
+  }
 }
